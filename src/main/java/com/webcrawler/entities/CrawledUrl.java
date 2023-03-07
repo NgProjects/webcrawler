6d82e72f4e26df525d863c6c920af4a078c773d7
Ng <erondung@gmail.com>
2023-03-07 11:42:55 +0000

    - Remove unused entity columns

--- src/main/java/com/webcrawler/entities/CrawledUrl.java
+++ src/main/java/com/webcrawler/entities/CrawledUrl.java
@@ -2,14 +2,17 @@ package com.webcrawler.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.cassandra.core.cql.Ordering;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
 
+import java.io.Serial;
 import java.io.Serializable;
-import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -19,21 +22,24 @@ import java.util.UUID;
 @Table
 public class CrawledUrl implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 7311593512278944531L;
+
     @PrimaryKeyColumn(name = "id", ordinal = 0, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
-    private UUID id;
+    private UUID id = UUID.randomUUID();
 
     @PrimaryKeyColumn(name = "url", ordinal = 1, type = PrimaryKeyType.PARTITIONED)
     private String url;
-    @Column
-    private boolean deleted = false;
 
-    @Column
-    private Timestamp dateCreated = new Timestamp(System.currentTimeMillis());
-
-    @Column
-    private Timestamp dateModified = new Timestamp(System.currentTimeMillis());
-
-    @Column
+    @Column("extracted_urls")
     private Set<String> extractedUrls = new HashSet<>();
+
+    @CreatedDate
+    @Column("create_date")
+    private Instant createDate;
+
+    @LastModifiedDate
+    @Column("last_modified_date")
+    private Instant lastModifiedDate;
 
 }
