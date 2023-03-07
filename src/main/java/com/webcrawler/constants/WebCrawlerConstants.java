28a3f803f22e793e924d081460c8e7dd2e56f0ae
Ng <erondung@gmail.com>
2023-03-07 09:54:38 +0000

    - removed unused constants

--- src/main/java/com/webcrawler/constants/WebCrawlerConstants.java
+++ src/main/java/com/webcrawler/constants/WebCrawlerConstants.java
@@ -5,6 +5,5 @@ public class WebCrawlerConstants {
     public static final String MOZILLA = "Mozilla";
     public static final String HREF_TAG = "href";
     public static final String CHILD_CACHE_KEY = "C_";
-    public static final String URL_CACHE_KEY = "U_";
-    public static final String CRAWL_LIMIT_CONFIG = "crawl.limit";
+
 }
